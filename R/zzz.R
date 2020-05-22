1ef3c6c3e31615b39e8f59cc4081f74fd87db6db
bobbeltje <matthiaswubs@gmail.com>
2020-05-22 13:11:09 +0200

    bug fix

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@ loc <- rappdirs::user_data_dir('snippie')
 .onLoad <- function(libname, pkgname){
   library(shiny)
   library(DT)
-  if (!dir.exists(loc)) dir.create(loc)
+  if (!dir.exists(loc)) dir.create(loc, recursive=TRUE)
   fname <- file.path(loc, 'data.csv')
   if (file.exists(fname)){
     d <- data.table::fread(fname)
